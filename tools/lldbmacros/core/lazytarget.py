406c081852c6c4ab2ebfb3b30ebd9bdced49a391
xkitz7 <xkitz75@gmail.com>
2025-11-25 01:44:39 +0300

    Remove non-arch directories and files per new architecture structure

--- tools/lldbmacros/core/lazytarget.py
+++
@@ -1,7 +0,0 @@
-""" Module to abstract lazy evaluation of lldb.SBTarget
-    for kernel
-"""
-from .caching import LazyTarget
-
-# backward compatibility, this has moved to caching
-__all__ = [ LazyTarget.__name__ ]
--- tools/lldbmacros/kmemory/__init__.py
+++
@@ -1,5 +0,0 @@
-from .kmem   import *
-from .btlog  import *
-from .whatis import *
-from .vm     import *
-from .zone   import *
