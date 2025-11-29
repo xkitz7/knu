6433d39925b253e8df698032adc26be7b615e727
XKITZ <xkitz75@gmail.com>
2025-11-29 14:36:44 +0300

    Create translate.cpp

---
+++ compat/linuxbin/translate.cpp
@@ -0,0 +1,11 @@
+class LinuxTranslators {
+public:
+    static int OpenFlagsLinuxToNative(int linux_flags) {
+        int native_flags = 0;
+        return native_flags;
+    }
+    
+    static int ErrorNativeToLinux(int native_error) {
+        return native_error;
+    }
+};
